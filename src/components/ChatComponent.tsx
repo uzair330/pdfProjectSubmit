7b8f58b45eb071c9d35140db8277c17c43a58695
uzair330 <uzair330@gmail.com>
2024-02-14 17:53:53 +0500

    initail commit

--- src/components/ChatComponent.tsx
+++ src/components/ChatComponent.tsx
@@ -57,36 +57,6 @@ const ChatComponent = ({ chatId }: Props) => {
     }
   }, [messages]);
   return (
-//     <div
-//       className="relative max-h-screen "
-//       id="message-container"
-//     >
-//       {/* header */}
-//       <div className=" h-fit">
-//         {/* <h3 className="text-xl font-bold fixed bg-slate-800 opacity-50 rounded-md text-white px-4">Chat</h3> */}
-//       </div>
-//       <ScrollArea className="h-400 w-full rounded-md border ">
-//       {/* message list */}
-//       <MessageList messages={messages} isLoading={isLoading} />
-// </ScrollArea> 
-
-      // <form
-      //   onSubmit={handleSubmit}
-      //   className="sticky bottom-0 inset-x-0 px-2 py-4  bg-white"
-      // >
-      //   <div className="flex mt-8">
-      //     <Input
-      //       value={input}
-      //       onChange={handleInputChange}
-      //       placeholder="chat with pdf..."
-      //       className="w-full"
-      //     />
-      //     <Button className="bg-blue-600 ml-2">
-      //       <Send className="h-4 w-4" />
-      //     </Button>
-      //   </div>
-      // </form>
-//     </div>
 <div className="">
 <div className="">
 <ScrollArea className="h-72 w-full rounded-md border ">
